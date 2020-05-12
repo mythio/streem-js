7f92d77e8333cb76c9bff90dbc8de8d00ebe9eb8
mythio <mythio.2909@gmail.com>
2020-05-12 14:36:31 +0530

    Chore: Class to FP

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,5 +1,5 @@
-import { injectable } from "inversify";
 import chalk from "chalk";
+import * as config from "./";
 
 enum LOG_LEVEL {
 	NONE = 0,
@@ -10,38 +10,31 @@ enum LOG_LEVEL {
 
 type Log = keyof typeof LOG_LEVEL;
 
-@injectable()
-export class Logger {
-	private _logLevel: LOG_LEVEL;
+const logLevel = LOG_LEVEL[config.LOG_LEVEL];
 
-	constructor(logLevel: LOG_LEVEL) {
-		this._logLevel = logLevel;
-	}
+const getLocaleTime = (): string => {
+	const date = new Date();
+	return `${date.toLocaleDateString()} ${date.toLocaleTimeString([], { hour12: false })}`;
+};
 
-	private getLocaleTime(): string {
-		const date = new Date();
-		return `${date.toLocaleDateString()} ${date.toLocaleTimeString([], { hour12: false })}`;
-	}
-
-	public log(logType: Log, ...args: string[]): void {
-		switch (LOG_LEVEL[logType]) {
-			case 1: {
-				if (this._logLevel < LOG_LEVEL.ERROR) return;
-				console.log(this.getLocaleTime, process.pid, chalk.bold.bgRedBright("E: "), ...args);
-				break;
-			}
-			case 2: {
-				if (this._logLevel < LOG_LEVEL.INFO) return;
-				console.log(this.getLocaleTime, process.pid, chalk.bold.bgGreenBright("I: "), ...args);
-				break;
-			}
-			case 3: {
-				if (this._logLevel < LOG_LEVEL.DEBUG) return;
-				console.log(this.getLocaleTime, process.pid, chalk.bold.bgBlueBright("D: "), ...args);
-				break;
-			}
-			default:
-				break;
+export const log = (logType: Log, ...args: string[]): void => {
+	switch (LOG_LEVEL[logType]) {
+		case 1: {
+			if (logLevel < LOG_LEVEL.ERROR) return;
+			console.log(getLocaleTime, process.pid, chalk.bold.bgRedBright("E: "), ...args);
+			break;
 		}
+		case 2: {
+			if (logLevel < LOG_LEVEL.INFO) return;
+			console.log(getLocaleTime, process.pid, chalk.bold.bgGreenBright("I: "), ...args);
+			break;
+		}
+		case 3: {
+			if (logLevel < LOG_LEVEL.DEBUG) return;
+			console.log(getLocaleTime, process.pid, chalk.bold.bgBlueBright("D: "), ...args);
+			break;
+		}
+		default:
+			break;
 	}
-}
+};
