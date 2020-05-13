1b4b671c478dc26fb5e3bbc6352ce71b021db46f
mythio <mythio.2909@gmail.com>
2020-05-13 14:10:41 +0530

    feat: Move to buffer pool for readable streams

---
+++ src/core/bufferPool.ts
@@ -0,0 +1,38 @@
+import { Readable } from "stream";
+
+export class BufferPool extends Readable {
+	private totalBufferLength: number;
+	private needBufferLength: number;
+	private gFun: any;
+
+	constructor(options) {
+		super(options);
+	}
+
+	init(gFun: any): void {
+		this.totalBufferLength = 0;
+		this.needBufferLength = 0;
+		this.gFun = gFun;
+		this.gFun.next();
+	}
+
+	push(buf: Buffer): boolean {
+		this.totalBufferLength += buf.length;
+		if (this.needBufferLength > 0 && this.needBufferLength <= this.totalBufferLength)
+			this.gFun.next();
+		return super.push(buf);
+	}
+
+	read(size: number): any {
+		this.totalBufferLength -= size;
+		return super.read(size);
+	}
+
+	need(size: number): boolean {
+		const ret = this.totalBufferLength < size;
+		if (ret) {
+			this.needBufferLength = size;
+		}
+		return ret;
+	}
+}
