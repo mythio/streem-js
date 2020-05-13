2268f1429087bd525e36cbe9bd17ee1800e300bd
mythio <mythio.2909@gmail.com>
2020-05-14 03:23:04 +0530

    chore: Conenction

--- src/rtmp/connection.ts
+++ src/rtmp/connection.ts
@@ -9,7 +9,7 @@ export default class Connection extends EventEmitter {
 	id: any;
 	socket: any;
 	conns: any;
-	consumers: Buffer[];
+	consumers: Connection;
 	producers: any;
 	rtmpStatus: number;
 	isStarting: boolean;
@@ -39,7 +39,7 @@ export default class Connection extends EventEmitter {
 		ppsLen: number;
 		pps: any;
 	};
-	sendBufferQueue: any[];
+	sendBufferQueue: Buffer[];
 
 	constructor(id, socket, conns, producers) {
 		super();
@@ -63,7 +63,9 @@ export default class Connection extends EventEmitter {
 		this.publishStreamName = "";
 
 		this.bp = new BufferPool(undefined);
-		this.bp._read = () => {};
+		this.bp._read = () => {
+			return;
+		};
 		this.bp.on("error", () => {});
 
 		this.parser = parseRtmpMessage(this);
@@ -86,17 +88,12 @@ export default class Connection extends EventEmitter {
 		this.sendBufferQueue = [];
 	}
 
-	generateSessionId(): void {
+	public run(): void {
 		this.isStarting = true;
 		this.bp.init(this.parser);
 	}
 
-	create(): void {
-		this.isStarting = true;
-		this.bp.init(this.parser);
-	}
-
-	stop() {
+	public stop(): void {
 		this.isStarting = false;
 		if (this.publishStreamName != "") {
 			// delete producers
@@ -109,7 +106,7 @@ export default class Connection extends EventEmitter {
 		this.emit("stop");
 	}
 
-	public getRealChunkSize(bodySize: number, chunkSize: number) {
+	private getRealChunkSize(bodySize: number, chunkSize: number) {
 		const length = bodySize + Math.floor(bodySize / chunkSize);
 		if (bodySize % chunkSize) {
 			return length;
@@ -118,7 +115,7 @@ export default class Connection extends EventEmitter {
 		}
 	}
 
-	public createRtmpMessage(header, body) {
+	private createRtmpMessage(header, body) {
 		const formatId = 0;
 		const bodySize = body.length;
 
@@ -176,7 +173,7 @@ export default class Connection extends EventEmitter {
 		return buffers;
 	}
 
-	public handleRtmpMessage(header, body: Buffer) {
+	private handleRtmpMessage(header, body: Buffer) {
 		switch (header.messageTypeID) {
 			case 0x01:
 				this.inChunkSize = body.readUInt32BE(0);
@@ -188,7 +185,7 @@ export default class Connection extends EventEmitter {
 				this.parseAudioMessage(header, body);
 				break;
 			case 0x09:
-				// @TODO Video message
+				// this.parseVideoMessage(header, body);
 				break;
 			case 0x0f:
 				// @TODO AMF0 Data
@@ -202,6 +199,196 @@ export default class Connection extends EventEmitter {
 			case 0x14:
 				// @TODO AMF0 Command
 				break;
+		}
+	}
+
+	private handleAmfCommandMessage(cmd: any) {
+		this.emit("command", cmd);
+
+		switch (cmd.cmd) {
+			case "connect": {
+				this.connectCmdObj = cmd.cmdObj;
+				this.app = this.connectCmdObj.app;
+				this.objectEncoding = cmd.cmdObj.objectEncoding != null ? cmd.cmdObj.objectEncoding : 0;
+				this.windowACK(5000000);
+				this.setPeerBandwidth(5000000, 2);
+				this.outChunkSize = 4096;
+				this.setChunkSize(this.outChunkSize);
+
+				this.emit("connect", cmd);
+				this.respondConnect();
+				break;
+			}
+			case "createStream":
+				this.respondCreateStream(cmd);
+				break;
+			case "play": {
+				const streamName = this.connectCmdObj.app + "/" + cmd.streamName;
+				this.playStreamName = streamName;
+
+				this.emit("play", cmd);
+				this.respondPlay();
+
+				if (!this.producers[streamName]) {
+					this.producers[streamName] = {
+						id: null,
+						consumers: {}
+					};
+				}
+
+				this.producers[streamName].consumers[this.id] = this;
+				this.startPlay();
+				break;
+			}
+			case "closeStream":
+				this.closeStream();
+				break;
+			case "deleteStream":
+				this.deleteStream();
+				break;
+			case "pause":
+				this.pauseOrUnpauseStream();
+				break;
+			case "releaseStream":
+				this.respondReleaseStream();
+				break;
+			case "FCPublish":
+				this.respondFCPublish();
+				break;
+			case "publish": {
+				const streamName = this.connectCmdObj.app + "/" + cmd.streamName;
+				if (!this.producers[streamName]) {
+					this.producers[streamName] = {
+						id: this.id,
+						consumers: {}
+					};
+				} else if (this.producers[streamName].id == null) {
+					this.producers[streamName].id = this.id;
+				} else {
+					this.respondPublishError();
+					return;
+				}
+
+				this.publishStreamName = streamName;
+				this.producer = this.producers[streamName];
+				this.consumers = this.producer.consumers;
+				this.emit("publish", cmd);
+				this.respondPublish();
+				break;
+			}
+			case "FCUnpublish":
+				this.respondFCUnpublish();
+				break;
+			default:
+				return;
+		}
+	}
+
+	private startPlay() {
+		const producer = this.producers[this.playStreamName];
+		if (
+			!producer.metadata ||
+			!producer.cacheAudioSequenceBuffer ||
+			!producer.cacheAudioSequenceBuffer
+		)
+			return;
+
+		const opt = {
+			cmd: "onMetaData",
+			cmdObj: producer.metaData
+		};
+
+		const body = amf.encode(opt);
+		const metadataMessage = this.createRtmpMessage(
+			{
+				chunkStreamId: 5,
+				timestamp: 0,
+				messageTypeId: 0x12,
+				messageStreamId: 1
+			},
+			body
+		);
+
+		const audioSequenceMessage = this.createRtmpMessage(
+			{
+				chunkStreamID: 4,
+				timestamp: 0,
+				messageTypeID: 0x08,
+				messageStreamID: 1
+			},
+			producer.cacheAudioSequenceBuffer
+		);
+
+		const videoSequenceMessage = this.createRtmpMessage(
+			{
+				chunkStreamID: 4,
+				timestamp: 0,
+				messageTypeID: 0x09,
+				messageStreamID: 1
+			},
+			producer.cacheVideoSequenceBuffer
+		);
+
+		const beginRtmpMessage = new Buffer("020000000000060400000000000000000001", "hex");
+		this.sendBufferQueue.push(beginRtmpMessage);
+		this.sendBufferQueue.push(metadataMessage);
+		this.sendBufferQueue.push(audioSequenceMessage);
+		this.sendBufferQueue.push(videoSequenceMessage);
+		this.sendRtmpMessage(this);
+	}
+
+	private sendRtmpMessage(self: this): void {
+		if (!self.isStarting) return;
+		const length = self.sendBufferQueue.length;
+		for (let i = 0; i < length; i++) {
+			self.socket.write(self.sendBufferQueue.shift());
+		}
+		setTimeout(self.sendRtmpMessage, 100, self);
+	}
+
+	private parseAudioMessage(header, body: Buffer): void {
+		// Found this code only for aac codec ;_;
+		console.log(header);
+		if (this.isFirstAudioReceived) {
+			const aacPacketType = body[1];
+			if (aacPacketType == 0) {
+				this.codec.aacProfile = body[2];
+				this.codec.aacSampleRate = body[3];
+
+				this.codec.aacChannels = (this.codec.aacSampleRate >> 3) & 0x0f;
+				this.codec.aacSampleRate =
+					((this.codec.aacProfile << 1) & 0x0e) | ((this.codec.aacSampleRate >> 7) & 0x01);
+				this.codec.aacProfile = (this.codec.aacProfile >> 3) & 0x1f;
+				this.codec.audioSampleRate = aacSampleRates[this.codec.aacSampleRate];
+				if (this.codec.aacProfile == 0 || this.codec.aacProfile == 0x1f) {
+					this.emit(
+						"error",
+						new Error(
+							"Parse audio aac sequence header failed," +
+								` adts object=${this.codec.aacProfile} invalid`
+						)
+					);
+				}
+				--this.codec.aacProfile;
+				this.isFirstAudioReceived = false;
+				this.producer.cacheAudioSequenceBuffer = new Buffer(rtmpBody);
+
+				for (const id in this.consumers) {
+					this.consumers[id].startPlay();
+				}
+			} else {
+				const sendRtmpHeader = {
+					chunkStreamID: 4,
+					timestamp: header.timestamp,
+					messageTypeID: 0x08,
+					messageStreamID: 1
+				};
+				const rtmpMessage = this.createRtmpMessage(sendRtmpHeader, rtmpBody);
+
+				for (const id in this.consumers) {
+					this.consumers[id].sendBufferQueue.push(rtmpMessage);
+				}
+			}
 		}
 	}
 }
